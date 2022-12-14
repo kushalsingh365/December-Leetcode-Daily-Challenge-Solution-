447301f76708316f1987ad1384fd39ee9b566bb8
Kushal Singh <81277166+kushalsingh365@users.noreply.github.com>
2022-12-14 14:44:15 +0530

    14_Dec_2022

---
+++ 14 - House Robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        if (len < 2) {
+            return nums[0];
+        }
+        
+        vector<int> dp(len, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        for (int i = 2; i < len; ++i) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[len - 1];
+    }
+};
